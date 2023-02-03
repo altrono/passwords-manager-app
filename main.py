9cce5e7e5805d64520dfd58d6a60322e790f97a7
altrono <hbotema16@gmail.com>
2023-02-03 19:44:00 +0200

    Added Search functionalities

--- main.py
+++ main.py
@@ -4,6 +4,29 @@ import random
 import string
 import pyperclip
 import json
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open('data.json') as data_file:
+            data = json.load(data_file)
+    except:
+        messagebox.showinfo(title='Error', message='No Data File found')
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title='Error', message=f'No details for {website} exists.')
+
+
+
+
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
@@ -46,9 +69,9 @@ def save():
             # Updating old data with new data
             data.update(new_data)
 
-            with open('data.json', 'a') as data_file:
+            with open('data.json', 'w') as data_file:
                 # Save Updated data
-                json.dump(new_data, data_file, indent=4)
+                json.dump(data, data_file, indent=4)
         finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
@@ -71,19 +94,21 @@ email_label.grid(row=2, column=0)
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=25)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
-email_entry = Entry(width=35)
+email_entry = Entry(width=43)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, 'hbotema1616@gmail.com')
-password_entry = Entry(width=21,)
+password_entry = Entry(width=25,)
 password_entry.grid(row=3, column=1)
 
 # Buttons
+search_button = Button(text='Search', width=14, command=find_password)
+search_button.grid(row=1, column=2)
 generate_password_button = Button(text="Generate Password", command=password_generator)
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36, command=save)
+add_button = Button(text="Add", width=37, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
